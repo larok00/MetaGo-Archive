ea4d7161e4c5eaf8e9a51e7f3fca135d83cf65b1
KH6 <Koral.Hassan@metaswitch.com>
2018-08-09 14:21:14 +0100

    Integrated front-end.

--- MetaGo_website/MetaGo/views.py
+++ MetaGo_website/MetaGo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. Welcome to MetaGo.")
+    context = {}
+    return render(request, 'MetaGo/index.html', context)
