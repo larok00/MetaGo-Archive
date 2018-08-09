3e106c1f36081e4cc6eab8df2eb66ec521877109
KH6 <Koral.Hassan@metaswitch.com>
2018-08-09 17:19:43 +0100

    Added POST request test.

--- MetaGo_website/MetaGo/views.py
+++ MetaGo_website/MetaGo/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
     context = {}
     return render(request, 'MetaGo/index.html', context)
+
+def photo_id(request):
+    if request.method == "POST":
+        raise KeyError(request.POST)
+    else:
+        return HttpResponseRedirect(reverse('MetaGo:index'))
